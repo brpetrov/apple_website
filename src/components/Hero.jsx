e2d242363ca6d72df28b25d592c52103d21d75a8
brpetrov <brpetrov@outlook.com>
2024-11-01 18:15:41 +0000

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+const Hero = () => {
+  useGSAP(() => {
+    gsap.to('.hero-title', {
+      opacity: 1,
+      duration: 0.5,
+    });
+  }, []);
+
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p className='hero-title'>iPhone 15 Pro</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
