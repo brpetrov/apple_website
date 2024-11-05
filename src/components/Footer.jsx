621ae0f6f93ce311b12a3cb1638a30c4b40149b4
brpetrov <brpetrov@outlook.com>
2024-11-05 18:01:50 +0000

    minor footer change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,10 +24,7 @@ const Footer = () => {
           <div className='flex'>
             {footerLinks.map((link, i) => (
               <p key={link} className='font-semibold text-gray text-xs'>
-                {link}{' '}
-                {i !== footerLinks.length - 1 && (
-                  <span className='mx-2'>|</span>
-                )}
+                {link} <span className=''> | </span>
               </p>
             ))}
           </div>
