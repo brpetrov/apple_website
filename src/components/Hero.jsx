1475c1a90624097d9e00d5cb0f62641183908e9b
brpetrov <brpetrov@outlook.com>
2024-11-05 17:41:33 +0000

    minor title change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
     <section className='w-full nav-height bg-black relative'>
       <div className='h-5/6 w-full flex-center flex-col'>
         <p id='hero' className='hero-title'>
-          iPhone 15 Pro
+          iPhone 15 Pro x Boyan Petrov
         </p>
         <div className='md:w-10/12 w-9/12'>
           <video
